5b427e409cb93ffe68fa643ba55c50845662273b
adrianritchie <ar@kodo.gg>
2018-08-25 20:32:30 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,239 @@
+#include <Arduino.h>
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+
+//#define BLYNK_PRINT Serial
+//#define BLYNK_DEBUG
+//#define BLYNK_SSL_USE_LETSENCRYPT
+#include <BlynkSimpleEsp8266.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS D6
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+BlynkTimer timer;
+
+//define your default values here, if there are different values in config.json, they are overwritten.
+char mqtt_server[40];
+char mqtt_port[6] = "8080";
+char blynk_token[34] = "BLYNK_AUTH_TOKEN";
+char virtual_pin[2] = "0";
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+void blynkTimerCallback() {
+ // put your main code here, to run repeatedly:
+
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+  Serial.print("Temperature for the device 1 (index 0) is: ");
+  Serial.println(sensors.getTempCByIndex(0));  
+
+    if (Blynk.connected()) {
+        Blynk.virtualWrite(atoi(virtual_pin), sensors.getTempCByIndex(0));  
+        Serial.println("Value sent to Blynk");
+    }
+    else {
+        Serial.println("Connecting to blynk");
+        Blynk.connect();
+    }
+}
+
+void setup() {
+  // start serial port
+  Serial.begin(9600);
+  Serial.println();
+
+
+    pinMode(D2, INPUT_PULLUP);
+  delay(5000);
+
+  if (digitalRead(D2) == LOW) {
+      Serial.println("Flash storage being formatted");
+    SPIFFS.format();
+  }
+  else {
+      Serial.println("Flash storage being retained");
+  }
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(blynk_token, json["blynk_token"]);
+          strcpy(virtual_pin, json["virtual_pin"]);
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+        configFile.close();
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
+  WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 32);
+  WiFiManagerParameter custom_virtual_pin("pin", "virtual pin", virtual_pin, 2);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //set static ip
+  //wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_blynk_token);
+  wifiManager.addParameter(&custom_virtual_pin);
+
+  //reset settings - for testing
+
+  if (digitalRead(D2) == LOW) {
+    Serial.println("Wifi settings being reset");
+    wifiManager.resetSettings();
+
+    bool ledState = HIGH;
+    pinMode(LED_BUILTIN, OUTPUT);  
+    while (digitalRead(D2) == LOW) {
+        digitalWrite(LED_BUILTIN, ledState);
+        ledState = (ledState == HIGH) ? LOW : HIGH;
+        delay(1000);
+    }
+    ESP.restart();
+    delay(5000);
+  }
+  else {
+    Serial.println("Wifi settings being retained");
+  }
+
+  
+
+  //set minimu quality of signal so it ignores AP's under that quality
+  //defaults to 8%
+  //wifiManager.setMinimumSignalQuality();
+  
+  //sets timeout until configuration portal gets turned off
+  //useful to make it all retry or go to sleep
+  //in seconds
+  //wifiManager.setTimeout(120);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("AutoConnectAP", "password")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
+  //read updated parameters
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(blynk_token, custom_blynk_token.getValue());
+  strcpy(virtual_pin, custom_virtual_pin.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+    json["blynk_token"] = blynk_token;
+    json["virtual_pin"] = virtual_pin;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+
+  Serial.println("local ip");
+  Serial.println(WiFi.localIP());
+
+
+  Blynk.config(blynk_token, mqtt_server, atoi(mqtt_port));
+
+    // Setup a function to be called every second
+  timer.setInterval(1000L, blynkTimerCallback);
+
+
+  // Start up the library
+  sensors.begin();
+}
+
+void loop() {
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+}
+
